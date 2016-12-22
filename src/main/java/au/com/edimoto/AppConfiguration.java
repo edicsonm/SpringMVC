b87cbb9e35b9f2ac79b6a6f10f04fd2d57a86f43
Edicson Morales <edicsonm@gmail.com>
2016-12-22 11:00:02 +1100

    Commit message

---
+++ src/main/java/au/com/edimoto/AppConfiguration.java
@@ -0,0 +1,20 @@
+package au.com.edimoto;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by torree on 22/12/2016.
+ */
+@Configuration
+@ComponentScan
+public class AppConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+}
